2a3c33f8ce081e4e03d6b75e0cdba291e9528a64
Haiko Schol <hs@haikoschol.com>
2024-06-22 14:38:32 +0700

    chore: move tracker impl to a separate file

---
+++ network/tracker.go
@@ -0,0 +1,64 @@
+package network
+
+import "container/list"
+
+type tracker struct {
+	length   int
+	messages *list.List
+	elemById map[string]*list.Element
+}
+
+func newTracker(length int) *tracker {
+	return &tracker{
+		length:   length,
+		messages: list.New(),
+		elemById: make(map[string]*list.Element),
+	}
+}
+
+func (t *tracker) Add(message *Message) error {
+	if _, ok := t.elemById[message.ID]; ok {
+		return nil
+	}
+
+	if t.messages.Len() == t.length {
+		front := t.messages.Front()
+		t.messages.Remove(front)
+		delete(t.elemById, front.Value.(*Message).ID)
+	}
+
+	t.elemById[message.ID] = t.messages.PushBack(message)
+	return nil
+}
+
+func (t *tracker) Delete(id string) error {
+	elem, ok := t.elemById[id]
+	if !ok {
+		return ErrMessageNotFound
+	}
+
+	t.messages.Remove(elem)
+	delete(t.elemById, id)
+	return nil
+}
+
+func (t *tracker) Message(id string) (*Message, error) {
+	elem, ok := t.elemById[id]
+	if !ok {
+		return nil, ErrMessageNotFound
+	}
+
+	return elem.Value.(*Message), nil
+}
+
+func (t *tracker) Messages() []*Message {
+	messages := make([]*Message, len(t.elemById))
+	i := 0
+
+	for e := t.messages.Front(); e != nil; e = e.Next() {
+		messages[i] = e.Value.(*Message)
+		i++
+	}
+
+	return messages
+}
